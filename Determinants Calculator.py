aab9c34788c8ecbeb83a680419b8fd935e9d1b2b
Bharnij <bharnijborah108@gmail.com>
2024-07-25 12:33:48 +0530

    Added PrettyTable  to display the determinants in a tabular format

--- Determinants Calculator.py
+++ Determinants Calculator.py
@@ -1,3 +1,6 @@
+
+from prettytable import PrettyTable
+
 print("Welcome to the determinants calculator!")
 
 matrix_type = int(input("What is the type of determinant? Type '2' for a 2x2 determinant and '3' for a 3x3 determinant"))
@@ -9,8 +12,16 @@ if matrix_type == 2:
     b1 = int(input("What is the number in 2nd row, 1st column?"))
     b2 = int(input("What is the number in 2nd row, 2nd column?"))   
 
+    table = PrettyTable()
+    table.field_names = ["X", "C1", "C2"]
+    table.add_row(["R1", a1, a2])
+    table.add_row(["R2", b1, b2])
+
+   
     det_2x2 = (a1 * b2 ) - (b1 * a2)
 
+    print("Your matrix is --> ")
+    print(table)
     print(f"The value of your determinant is {det_2x2}")
 
 else:
@@ -25,8 +36,14 @@ else:
     r3c2 = int(input("What is the number in 3rd row, 2nd column?"))
     r3c3 = int(input("What is the number in 3rd row, 3rd column?"))
 
+    table = PrettyTable()
+    table.field_names = ["X", "C1", "C2", "C3"]
+    table.add_row(["R1", r1c1, r1c2, r1c3])
+    table.add_row(["R2", r2c1, r2c2, r2c3])
+    table.add_row(["R3", r3c1, r3c2, r3c3])
+
     det_3x3 = r1c1 * (r2c2 * r3c3 - r3c2 * r2c3) - r1c2 * (r2c1 * r3c3 - r3c1 * r2c3) + r1c3 * (r2c1 * r3c2 - r3c1 * r2c2)
 
-    
-    
+    print("Your matrix is --> ")
+    print(table)
     print(f"The value of your determinant is {det_3x3}")
